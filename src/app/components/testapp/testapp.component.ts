c38a14f6f93d128b1dfbec04f6bf536d3e2a4897
root <root@iPhone>
2021-03-22 18:06:48 -0400

    js addded

--- src/app/components/testapp/testapp.component.ts
+++ src/app/components/testapp/testapp.component.ts
@@ -16,6 +16,11 @@ export class TestappComponent implements OnInit {
    public _uw:UserWService,
   private dataApi: DataApiService
      ) { }
+
+    loadAPI = null;  
+
+  url = "assets/assetspenguins/js/owl.js";
+  url2 = "assets/assetspenguins/js/script.js";
      public tixs:TixInterface;
   getAllTixsInitload(){
     this.dataApi
@@ -108,8 +113,31 @@ cartCalculate(){
     }
   }
 }
-
+ public loadScript() {
+    let node = document.createElement("script");
+    node.src = this.url;
+    node.type = "text/javascript";
+    node.async = true;
+    node.charset = "utf-8";
+    document.getElementsByTagName("head")[0].appendChild(node);
+  }
+  public loadScript2() {
+    let node = document.createElement("script");
+    node.src = this.url2;
+    node.type = "text/javascript";
+    node.async = true;
+    node.charset = "utf-8";
+    document.getElementsByTagName("head")[0].appendChild(node);
+  }
   ngOnInit() {
+      if (this._uw.loaded==true){
+      this.loadAPI = new Promise(resolve => {
+        this.loadScript();
+        this.loadScript2();
+        // this.loadScript3();
+        });
+      }
+    this._uw.loaded=true;
      // this.getAllTixsInitload();
      this.getTamanoIni();
      this.loadmore();
